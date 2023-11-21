8b35a58492b99642d280da1ea806cb824ad75da3
Matthew Lomando <80913065+twinblasterrex@users.noreply.github.com>
2023-11-20 21:25:20 -0500

    Pneumatics hub

--- src/main/java/frc/robot/IO/IO.java
+++ src/main/java/frc/robot/IO/IO.java
@@ -2,10 +2,12 @@ package frc.robot.IO;
 
 import edu.wpi.first.wpilibj.XboxController;
 import frc.lib.StadiaController;
+import frc.robot.commands.DriveCommand;
+
 import java.util.HashMap;
 import java.util.function.Supplier;
 
-public class IO {
+public abstract class IO extends DriveCommand {
     private static final XboxController primary = new XboxController(0);
     private static final StadiaController secondary = new StadiaController(1);
 
